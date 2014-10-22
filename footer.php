41545b8a06e86af068301e2f751e64f778d21666
Sheelah Brennan <slb53@hotmail.com>
2014-10-22 15:03:26 -0600

    Change footer text, add basic footer styling, remove unneeded layout styles

--- footer.php
+++ footer.php
@@ -12,11 +12,13 @@
 </div><!-- .row -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'great-outdoors' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'great-outdoors' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'great-outdoors' ), 'Great Outdoors', '<a href="http://sheelahb.com" rel="designer">Sheelah Brennan</a>' ); ?>
-		</div><!-- .site-info -->
+		<div class="row">
+			<div class="small-12 columns" data-equalizer-watch>
+				<p class="text-center">
+					&copy; <?php echo date('Y'); ?> <a href="http://sheelahb.com">Sheelah Brennan</a>
+				</p>
+			</div><!-- .columns -->
+		</div><!-- .row -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
