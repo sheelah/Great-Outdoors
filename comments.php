14710f985b570fd6a55415932c0e9bed9aca52d8
Sheelah Brennan <slb53@hotmail.com>
2014-10-30 18:20:22 -0600

    Start formatting comment appearance

--- comments.php
+++ comments.php
@@ -25,7 +25,7 @@ if ( post_password_required() ) {
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'great-outdoors' ),
+				printf( _nx( 'One comment:', '%1$s comments:', get_comments_number(), 'comments title', 'great-outdoors' ),
 					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
 			?>
 		</h2>
