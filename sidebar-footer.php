a222ad877e440d9eb8dc30426a8c65990f13dc6a
Sheelah Brennan <slb53@hotmail.com>
2014-11-04 00:19:51 -0700

    Use single-col block grid layout for footer widgets @ narrow widths, remove some unneeded _s styles

--- sidebar-footer.php
+++ sidebar-footer.php
@@ -9,7 +9,7 @@ if (!is_active_sidebar('sidebar-2')) {
 ?>
 
 <div class="row">
-	<ul id="footer-widgets" class="footer-widgets widget-area small-block-grid-3" role="complementary">
+	<ul id="footer-widgets" class="footer-widgets widget-area small-block-grid-1 medium-block-grid-2 large-block-grid-3" role="complementary">
 		<?php dynamic_sidebar('sidebar-2'); ?>
 	</ul>
 </div><!-- .row -->
