da6b49656a73f9787714b5b9e88f215d1be89c38
Sheelah Brennan <slb53@hotmail.com>
2015-04-01 16:03:52 -0600

    Remove picturefill locally and use CDN instead; use google CDN for jquery by default but add fallback; set version to v1.0.0; enqueue jquery at end of body; don't include date in grunt's banner text

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,6 @@ module.exports = function(grunt) {
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
     banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
       '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
       '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
       ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
--- lib/functions/enqueue-scripts.php
+++ lib/functions/enqueue-scripts.php
@@ -4,16 +4,26 @@
 */
 
 function great_outdoors_scripts() {
-	wp_enqueue_style( 'great-outdoors-style', get_stylesheet_uri() );
+	// Don't use the jQuery shipped with WordPress
+	if( !is_admin() ) {
+		wp_deregister_script( 'jquery' );
+
+		// Enqueue jQuery from Google CDN
+		wp_register_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js', array(), null, true );
+		add_filter('script_loader_src', 'great_outdoors_jquery_local_fallback', 10, 2);
+	}
+
+	wp_enqueue_script('jquery');
+	wp_enqueue_style( 'great-outdoors-style', get_stylesheet_uri(), array(), null );
 
 	$font_base_url = '//fonts.googleapis.com/css';
 	$font_query     = '?family=Open+Sans:400,700,800,400italic,700italic,800italic|PT+Serif:400,700,400italic,700italic|Roboto+Slab:400';
-	wp_enqueue_style( 'great-outdoors-google-fonts', $font_base_url . $font_query );
+	wp_enqueue_style( 'great-outdoors-google-fonts', $font_base_url . $font_query, array(), null );
 
-	wp_enqueue_style( 'great-outdoors-font-awesome', 'http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css' );
+	wp_enqueue_style( 'great-outdoors-font-awesome', 'http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css', array(), null );
 
 	// Enqueue concatenated and minified JS
-	wp_enqueue_script( 'picturefill', get_template_directory_uri() . '/js/lib/picturefill.min.js', array(), null );
+	wp_enqueue_script( 'picturefill', '//cdnjs.cloudflare.com/ajax/libs/picturefill/2.3.0/picturefill.min.js', array(), null );
 	wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/bower_components/modernizr/modernizr.js', array(), null, true );
 
 	wp_enqueue_script( 'foundation', get_template_directory_uri() . '/bower_components/foundation/js/foundation.min.js', array( 'jquery' ), null, true );
@@ -25,6 +35,22 @@ function great_outdoors_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'great_outdoors_scripts' );
+
+// http://wordpress.stackexchange.com/a/12450
+function great_outdoors_jquery_local_fallback( $src, $handle = null ) {
+	static $add_jquery_fallback = false;
+
+	if ( $add_jquery_fallback ) {
+		echo '<script>window.jQuery || document.write(\'<script src="' . $add_jquery_fallback .'"><\/script>\')</script>' . "\n";
+		$add_jquery_fallback = false;
+	}
+	if ( $handle === 'jquery' ) {
+		$add_jquery_fallback = apply_filters( 'script_loader_src', get_template_directory_uri() . '/js/lib/jquery-2.1.3.min.js', 'jquery-fallback');
+	}
+
+	return $src;
+}
+add_action('wp_head', 'great_outdoors_jquery_local_fallback');
 
 function great_outdoors_enqueue_editor_styles() {
 	// This theme styles the visual editor to resemble the theme style.
